faad6507ffe818b5de9bd9e74058d316dd2187cf
coutts <andrewjohncoutts@gmail.com>
2011-06-22 10:07:47 -0400

    Initial porting to 400plus.. "1000plus"  :)

---
+++ init.c
@@ -0,0 +1,369 @@
+#include "main.h"
+
+#include "init.h"
+
+extern void COPY();
+
+void my_romStart(int startType);
+int  my_usrInit(int startType);
+int  my_usrRoot(char* pMemPoolStart, unsigned int memPoolSize);
+
+void my_taskcreate_Startup();
+void my_task_Startup();
+int  my_InitializeIntercom();
+
+void COPY()
+{
+	int i;
+	
+	long *from = (long*) 0x800000;
+	long *to   = (long*) 0x7F0000;
+	
+	for (i = 0; i < 0x4000; i++) {
+		to[i] = from[i];
+	}
+}
+
+// entry routine, entry.S calls this, so we enter here after power up.
+void my_romStart(int startType)
+{
+	unknown_cache(&off_FFD51890, &addr_0x1900, 0x35f40 >> 2);
+	my_usrInit(startType);
+}
+
+int my_usrInit(int startType)
+{
+	sysStart()
+	cacheLibInit(1, 2);
+	excVecInit();
+	sysHwInit();
+	sub_FF810B54();
+	my_usrInit_1();
+}
+
+
+void my_usrInit_1()
+{
+	classLibInit();
+	taskLibInit();
+	qInit(&readyQHead, qPriBMapClassId, &readyQBMap, 0x100); 
+	qInit(&activeQHead, qFifoClassId);
+	qInit(&tickQHead,   qPriListClassId);
+	workQInit();
+	sub_FF810504();
+	kernelInit(my_usrRoot, 0x4000, &off_37840, sysMemTop(), 0xC00);
+	return 0;
+}
+
+int my_sub_FF811004(char* pMemPoolStart, unsigned int memPoolSize)
+{
+	nullsub_307();
+	semBLibInit();
+	semMLibInit();
+	semCLibInit();
+	semQLibInit();
+	wdLibInit();
+	taskHookInit();	
+}
+
+int my_usrRoot(char* pMemPoolStart, unsigned int memPoolSize)
+{
+	my_sub_FF811004(pMemPoolStart, memPoolSize);
+	memInit(pMemPoolStart, memPoolSize);
+	memPartLibInit(pMemPoolStart, memPoolSize);
+	usrMmuInit();
+	sysClockInit();
+	selectInit(0x32);
+	usrRoot_init0();
+	usrRoot_init1();
+	usrRoot_init2();
+	selTaskDeleteHookAdd();
+	sub_FFD50CC0();
+	my_taskcreate_Startup();
+	return 0;
+}
+
+
+void my_taskcreate_Startup() {
+	CreateMainHeap(0x200000, 0x800000 - 0x10000); // in end of MainHeap - own code - 64 Kb
+	
+	sub_FFB0FF74();
+	sub_FFB2E108(0x386D4380);
+	
+	EnableDispatch();
+	CreateTask("Startup", 0x19, 0x2000, my_task_Startup, 0);
+}
+
+void my_task_Startup() {
+	DebugManager(1, 0x1F, 0x180000, 0x40000, 0x1C0000);
+	
+	dmstart();
+	dmProcInit();
+	
+	initialize(); // task_dispatcher
+	
+	sub_FFAFE5BC();
+	SetAssert();
+	EventProcedureServiceInit();
+	ShutDownProcInit();
+	Install3VMemory(0xF8000000);
+	RomManagerInit();
+	CreateParamPubInstance();
+	PropertyServiceInit();
+	ErrorNumberListInit();
+	FatalErrorsProcInit();
+	RegisterISRs_OCH();
+	BlockUntilAfterTimeoutProcInit(50);
+	
+	sub_FFB07740(0x10, 8, 0x1BBC);
+	ResourceNameServiceInit();
+	
+	MemorySuite(0);
+	
+	sysClockRateSet_100(3);
+	
+	sub_FFB2BD6C();
+	
+	InitializeSerialIO();
+	
+	RtcInit(0x386D4380);
+	
+	AdjDefectsInit();
+	
+	CameraAdjsInit();
+	
+	SetAssertProc(AssertPrepare, 0);
+	
+	my_InitializeIntercom(); // InitializeIntercom();
+	
+	AfeGainCmosParamInit();
+	
+	EngineInit();
+	
+	EDmacPriorityManager();
+	
+	EngineResourceInit();
+	
+	PowerMgrInit(0);
+	
+	ClockInit(1);
+	
+	RegisterISR_CAPREADY();
+	
+	FaceSensorInit();
+	
+	RemDrvInit();
+	ActSweepInit();
+	
+	LcdInit();
+	
+	DisplayInit1();
+	
+	DisplayInit2();
+	
+	PowerSaveProcInit();
+	
+	sub_FFA03B0C();
+	
+	sub_FFA05114();
+	
+	InitializeImagePlayDriver();
+	
+	LensNameTblInit();
+	
+	LensPOTblInit();
+	
+	FlyingInit();
+	
+	CaptureInit();
+	
+	BathtubSaturateInit();
+	
+	Module_CaptureImagePass();
+	
+	ClearSomeCapMem();
+	
+	ColorAdjustmentsInit();
+	
+	Module_PreDarkPassInit();
+	
+	LoadSystemInfo();
+	
+	SharedBufferInit(0x10800000, 0x18000000, 0xEE0000, 0xEE0000);
+	
+	FileCacheInit();
+	PackMemInit();
+	
+	ImagePropInit();
+	DigPropInit();
+	
+	ShootMainInit();
+	
+	OlcInfoInit();
+	
+	RegisterISR_EMERGENCY_CARDDOOR();
+	
+	MainCtrlInit();
+	
+	CaptureSemaphoreInit();
+	
+	VShadingInit();
+	
+	Module_CaptureDarkPassInit();
+	
+	Module_DarkSubtractionPassInit();
+	BathtubInit();
+	
+	Module_BathtubCorrectPassInit();
+	
+	Module_VObIntegPassInit();
+	
+	SetProjectionInit();
+	Module_DefectsDetectPassInit();
+	
+	DefsInit();
+	WbDetectionInit();
+	ObInit();
+	
+	Module_WbDetectionPassInit();
+	DefsProcInit();
+	
+	Module_ObAreaCopyPassInit();
+	Module_AdditionVTwoLinePassInit();
+	
+	VShadingProcInit();
+	Module_VShadingCorrectPassInit();
+	
+	sub_FFA24838();
+	HuffmanInit();
+	
+	RawToJpegPass_L_Init();
+	RawToJpegPass_M2_Init();
+	
+	RawToJpegPass_S_Init();
+	YcToJpegLargeFastInit();
+	
+	YcToJpegM2FastInit();
+	YcToJpegSFastInit();
+	
+	RawToLosslessInit();
+	Module_YcToTwainInit();
+	
+	RawToYcPass_S_Init();
+	RawToYPackPass_S_Init();
+	
+	DvlpInit();
+	DecodeJpegPassInit();
+	HistPassInit();
+	RectangleColorPassInit();
+	RectangleCopyPassInit();
+	
+	ResizeYuvPassInit();
+	sub_FFA35354();
+	LpfPassInit();
+	
+	EncodeJpegPassInit();
+	AdjRgbGainInit();
+	LuckyInit();
+	
+	SysInfoProcInit();
+	
+	TablesInit();
+	
+	ColorInit();
+	
+	CtrlManRecursiveLock();
+	
+	CtrlSrvInit(0x19);
+	
+	LangConInit();
+	sub_FF926E40();
+	
+	CreateDispSwControlPubInstance();
+	
+	CreateMemoryManagerPubInstance();
+	
+	my_GUIInit(); //GUIInit();
+	GUIApiCalls();
+	
+	InitializeImagePlayer();
+	
+	ColorBarProcsInit();
+	LcdAdjustProcsInit();
+	
+	sub_FFB29348();
+	CMOSParamInit();
+	
+	CameraSettingsInit();
+	BootDiskProcsInit();
+	
+	DDDInit();
+	TFTInit();
+	
+	RegisterResourceName(hResourceName, "USR ROOT DEVICE HANDLE", 0x7B);
+	
+	RegisterResource_env(0xC02200B8, "U2VBUS");
+	RegisterResource_env(1, "USBC20 VBUS SUPPORT");
+	
+	RegisterResource_env(0x14, "DEVICESPEED");
+	USBC20_Init();
+	USBC20_USBIF_Init();
+	
+	USBC20_BUFCON_Init();
+	USBC20_CLK_Init();
+	USBC20_HDMAC_Init();
+	
+	DCPClassFunctionsInit();
+	USBDriverInit();
+	RapiSwitcherInit();
+	
+	DCPClassInit();
+	RAPITransportUSBInit();
+	PTPRespondInit();
+	
+	PTPFrameworkInit();
+	StartupPtpResponder();
+	
+	RapiTransportManagerInit();
+	DCPClassInit();
+	
+	EventProcServerInit();
+	sub_FFA5D8A0();
+	DCPInit();
+	
+	SesnMngrInit();
+	MemMngrInit();
+	InitializeRapiTransportManager();
+	
+	PrintInit();
+	sub_FF95EC54();
+	SomePrintInit();
+	sub_FF9EB94C();
+	
+	InitializeUSBDriver();
+	TransMemoryInit();
+	InitializeComCtrl();
+	
+	FactoryModeInit();
+	DP_Init(0, 0x1B, 0, 0);
+	return_0();
+	
+	sub_FF98CF4C();
+	EdLedProcsInit();
+	CallBacksInit();
+	
+	RegistNotifyConnectDT();
+	DPOF_Initialize();
+	
+	MpuMonInit();
+	
+	StartConsole();
+}
+
+int my_InitializeIntercom() {
+	printf("InitializeIntercom\n");
+	InitIntercomData(intercom_proxy);
+	CreateIntercomSem();
+	
+	return 0;
+}
--- main.c
+++ main.c
@@ -1,88 +1,271 @@
+#include "firmware.h"
+
+#include "af_patterns.h"
+#include "display.h"
+#include "info.h"
+#include "languages.h"
+#include "menu.h"
+#include "menu_rename.h"
+#include "menu_settings.h"
+#include "presets.h"
+#include "tasks.h"
+#include "viewfinder.h"
+
 #include "main.h"
 
-#define LED_RED     0xC02200E0
-#define LED_BLUE    0xC02200E8
+// Camera data
+type_CAMERA_MODE *cameraMode = (type_CAMERA_MODE*)&DPData;
 
-void dumpramrom()
-{
-	FIO_RemoveFile("B:/RAMDUMP.BIN");
-	int f = FIO_CreateFile("B:/RAMDUMP.BIN");
-	
-	if (f!=-1) { 
-		FIO_WriteFile(f, (void*)0xFFFF0000, 4);
-		FIO_WriteFile(f, (void*)4, 0x1900-4);
-		FIO_WriteFile(f, (void*)0x1900, 32*1024*1024-0x1900);
-		
-		FIO_CloseFile(f);
+// Main message queue
+int *message_queue;
+
+// Global status
+type_STATUS status = {
+	button_down       : FALSE,
+	script_running    : FALSE,
+	iso_in_viewfinder : FALSE,
+	afp_dialog        : FALSE,
+	last_preset       : FALSE,
+	ignore_ae_change  : FALSE,
+	booting           : TRUE,
+};
+
+// Action definitions
+type_ACTION actions_main[]  = {
+	{IC_BUTTON_UP,    TRUE,  FALSE,  {restore_iso}},
+	{IC_BUTTON_DOWN,  TRUE,  FALSE,  {restore_wb}},
+	{IC_BUTTON_LEFT,  TRUE,  FALSE,  {restore_metering}},
+	{IC_BUTTON_DP,    FALSE, TRUE,   {dp_action}},
+	{IC_BUTTON_AV,    TRUE,  FALSE,  {toggle_raw_jpeg}},
+	END_OF_LIST
+};
+
+type_ACTION actions_menu[]  = {
+	{IC_BUTTON_DP,    FALSE, TRUE, {menu_settings_start}},
+	END_OF_LIST
+};
+
+type_ACTION actions_info[]  = {
+	{IC_BUTTON_SET,   FALSE, TRUE, {info_option_next}},
+	{IC_BUTTON_DOWN,  TRUE,  TRUE, {info_option_next}},
+	{IC_BUTTON_RIGHT, TRUE,  TRUE, {info_option_next}},
+	{IC_BUTTON_UP,    TRUE,  TRUE, {info_option_prev}},
+	{IC_BUTTON_LEFT,  TRUE,  TRUE, {info_option_prev}},
+	END_OF_LIST
+};
+
+type_ACTION actions_400plus[]  = {
+	{IC_BUTTON_RIGHT,      TRUE,  TRUE,  {menu_right}},
+	{IC_BUTTON_LEFT,       TRUE,  TRUE,  {menu_left}},
+	{IC_BUTTON_AV,         TRUE,  TRUE,  {menu_cycle}},
+	{IC_BUTTON_SET,        FALSE, TRUE,  {menu_event_set}},
+	{IC_BUTTON_DP,         FALSE, TRUE,  {menu_event_dp}},
+	{IC_BUTTON_DIAL_LEFT,  FALSE, TRUE,  {menu_submenu_prev}},
+	{IC_BUTTON_DIAL_RIGHT, FALSE, TRUE,  {menu_submenu_next}},
+	{IC_DIALOGOFF,         FALSE, FALSE, {menu_event_close}},
+	END_OF_LIST
+};
+
+type_ACTION actions_rename[]  = {
+	{IC_BUTTON_RIGHT,      TRUE,  TRUE,  {rename_right}},
+	{IC_BUTTON_LEFT,       TRUE,  TRUE,  {rename_left}},
+	{IC_BUTTON_AV,         TRUE,  TRUE,  {rename_cycle}},
+	{IC_BUTTON_SET,        FALSE, TRUE,  {rename_action}},
+	{IC_BUTTON_DIAL_LEFT,  FALSE, TRUE,  {rename_prev}},
+	{IC_BUTTON_DIAL_RIGHT, FALSE, TRUE,  {rename_next}},
+	{IC_DIALOGOFF,         FALSE, FALSE, {rename_event_close}},
+	END_OF_LIST
+};
+
+type_ACTION actions_meter[] = {
+	{IC_BUTTON_DP,    FALSE, TRUE, {set_metering_spot}},
+	END_OF_LIST
+};
+
+type_ACTION actions_wb[] = {
+	{IC_BUTTON_DP,    FALSE, TRUE, {set_whitebalance_colortemp}},
+	END_OF_LIST
+};
+
+type_ACTION actions_iso[] = {
+	{IC_BUTTON_DP,    FALSE, TRUE, {set_iso_high}},
+	END_OF_LIST
+};
+
+type_ACTION actions_face[] = {
+	{IC_BUTTON_UP,    TRUE, TRUE, {}},
+	{IC_BUTTON_DOWN,  TRUE, TRUE, {}},
+	{IC_BUTTON_RIGHT, TRUE, TRUE, {viewfinder_right, viewfinder_end}},
+	{IC_BUTTON_LEFT,  TRUE, TRUE, {viewfinder_left,  viewfinder_end}},
+	END_OF_LIST
+};
+
+type_ACTION actions_af[] = {
+	{IC_BUTTON_SET,   FALSE, TRUE, {afp_center}},
+	{IC_BUTTON_UP,    TRUE,  TRUE, {afp_top}},
+	{IC_BUTTON_DOWN,  TRUE,  TRUE, {afp_bottom}},
+	{IC_BUTTON_RIGHT, TRUE,  TRUE, {afp_right}},
+	{IC_BUTTON_LEFT,  TRUE,  TRUE, {afp_left}},
+	END_OF_LIST
+};
+
+type_CHAIN intercom_chains[] = {
+	{GUIMODE_OLC,       actions_main},
+	{GUIMODE_MAIN,      actions_main},
+	{GUIMODE_MENU,      actions_menu},
+	{GUIMODE_INFO,      actions_info},
+	{GUIMODE_400PLUS,   actions_400plus},
+	{GUIMODE_RENAME,    actions_rename},
+	{GUIMODE_METER,     actions_meter},
+	{GUIMODE_WB,        actions_wb},
+	{GUIMODE_ISO,       actions_iso},
+	{GUIMODE_FACE,      actions_face},
+	{GUIMODE_AFPATTERN, actions_af},
+	END_OF_LIST
+};
+
+void task_dispatcher();
+
+void initialize() {
+	message_queue = (int*)CreateMessageQueue("message_queue", 0x40);
+	CreateTask("Task Dispatcher", 25, 0x2000, task_dispatcher, 0);
+
+	ENQUEUE_TASK(start_up);
+}
+
+void initialize_display() {
+	ENQUEUE_TASK(restore_display);
+}
+
+void intercom_proxy(const int handler, char *message) {
+	int gui_mode;
+	int message_len = message[0];
+	int event       = message[1];
+	int param       = message_len > 1 ? message[2] : FALSE;
+	int holds       = param;
+
+	type_CHAIN  *chain;
+	type_ACTION *action;
+
+	// Status-independent events and special cases
+	switch (event) {
+	case IC_MAIN_DIAL: // Mode dial moved
+		// Ignore first AE change after loading a preset, as it generates this same event
+		if (status.ignore_ae_change) {
+			status.ignore_ae_change = FALSE;
+		} else {
+			status.last_preset  = FALSE;
+			status.main_dial_ae = param;
+			if (presets_config.use_adep && status.main_dial_ae == AE_MODE_ADEP) {
+				if (status.booting) {
+					ENQUEUE_TASK(preset_recall);
+				} else {
+					ENQUEUE_TASK(preset_recall_full);
+				}
+			}
+		}
+		goto pass_message;
+	case IC_SETTINGS: // Settings changed
+		// Restore display
+		ENQUEUE_TASK(restore_display);
+		goto pass_message;
+	case IC_DIALOGON: // Entering a dialog
+		status.afp_dialog = (param == IC_SET_AF);
+		goto pass_message;
+	case IC_AFPDLGOFF: // Exiting AF-Point selection dialog
+		if (status.afp_dialog) {
+			// Open Extended AF-Point selection dialog
+			message[1] = IC_AFPDLGON;
+			status.afp_dialog = FALSE;
+			ENQUEUE_TASK(afp_enter);
+		}
+		goto pass_message;
+	case IC_SET_LANGUAGE:
+		ENQUEUE_TASK(lang_pack_config);
+		goto pass_message;
+	case IC_BUTTON_DIAL: // Front Dial, we should detect direction and use our BTN IDs
+		event = (param & 0x80) ? IC_BUTTON_DIAL_LEFT : IC_BUTTON_DIAL_RIGHT;
+		holds = FALSE;
+		break;
+	case IC_BUTTON_DP: // DP Button while a script is running
+		if (status.script_running) {
+			status.script_running = FALSE;
+			goto block_message;
+		}
+		break;
 	}
-	
-	f = FIO_OpenFile("B:/ROMDUMP.BIN", 0, 0644);
-	FIO_CloseFile(f);
-	
-	if (f==-1) { 
-		*((volatile long*)LED_BLUE) = 0x46;
-		f = FIO_CreateFile("B:/ROMDUMP.BIN");		
-		FIO_WriteFile(f, (void*)0xFF800000, 8*1024*1024);
-		FIO_CloseFile(f);
-		*((volatile long*)LED_BLUE) = 0x44;
+
+	// Check for button-up events, even if the current GUI mode does not match
+	if (status.button_down && status.button_down == event && !holds) {
+		status.button_down = FALSE;
+
+		// Launch the defined task
+		if (status.button_up_task)
+			ENQUEUE_TASK(status.button_up_task);
+
+		// Decide how to respond to this button
+		if (status.button_up_block)
+			goto block_message;
+		else
+			goto pass_message;
+	}
+
+	// Use fictitious GUI modes so everything else fits nicely
+	if (FLAG_FACE_SENSOR && FLAG_GUI_MODE == GUIMODE_MAIN)
+		gui_mode = GUIMODE_FACE;
+	else
+		gui_mode = FLAG_GUI_MODE;
+
+	// Loop over all the action chains
+	for(chain = intercom_chains; ! IS_EOL(chain); chain++) {
+
+		// Check whether this action chain corresponds to the current GUI mode
+		if (chain->type == gui_mode) {
+
+			// Loop over all the actions from this action chain
+			for (action = chain->actions; ! IS_EOL(action); action++) {
+
+				// Check whether this action corresponds to the event received
+				if (action->button == event) {
+
+					// Consider buttons with "button down" and "button up" events
+					// and save "button up" parameters for later use
+					if (action->holds && holds) {
+						status.button_down     = event;
+						status.button_up_task  = action->task[1];
+						status.button_up_block = action->block;
+					}
+
+					// Launch the defined task
+					if (action->task[0])
+						ENQUEUE_TASK(action->task[0]);
+
+					// Decide how to respond to this button
+					if (action->block)
+						goto block_message;
+					else
+						goto pass_message;
+				}
+			}
+
+			// Once we find a matching action chain, we look no futher
+			break;
+		}
+	}
+
+pass_message:
+	IntercomHandler(handler, message);
+
+block_message:
+	return;
+}
+
+void task_dispatcher () {
+	type_TASK task;
+
+	// Loop while receiving messages
+	for (;;) {
+		ReceiveMessageQueue(message_queue, &task, 0);
+		task();
 	}
 }
-
-void MyTask2()
-{
-	/*while (1) {
-	 SleepTask(500);
-	 *((volatile long*)LED_BLUE) = 0x46;
-	 SleepTask(500);
-	 *((volatile long*)LED_BLUE) = 0x44;
-	 }*/
-	
-	/*  SleepTask(15000);
-	 dumpf();
-	 */ SleepTask(5000);
-	*((volatile long*)LED_BLUE) = 0x46;
-	
-	
-	// EnableBootDisk()  
-	/*Funktion f = 0xFFD21248;
-	 f();
-	 *((volatile long*)LED_BLUE) = 0x46;
-	 Das oben führt EnableBootDisk() aus.
-	 Wenn man eine normale SD-Karte reinsteckt, läuft alles ganz normal.
-	 Wenn man eine SD-Karte reinsteckt mit EOS_DEVELOP (?) und BOOTDISK, dann
-	 muss auf der Karte eine gültige FIR-Datei mit dem Namen "AUTOEXEC.BIN" sein,
-	 sonst ist die Kamera tot. Scheinbar wird die aber schon ausgeführt, sobald
-	 man den Deckel zumacht? Und wenn man sie rausnimmt, hängt sich die Kamera
-	 auf, auch wenn sie auf OFF war?
-	 Er führt die AUTOEXEC.BIN auch aus, wenn man die Karte nicht schreibgeschützt
-	 hatte?!
-	 */
-	
-	/*
-	 NEU:
-	 Nach dem Ausführen der Funktion muss man scheinbar einmal den Akku rausnehmen,
-	 damit das richtig funktioniert...
-	 Wenn man eine nicht bootfähige SD-Karte reinsteckt, passiert gar nix...
-	 Steckt man eine bootfähige mit einer ungültigen AUTOEXEC.BIN rein, beginnt
-	 sofort die blaue LED zu leuchten.
-	 Steckt man eine bootfähige mit einer FIR-Datei rein, die AUTOEXEC.BIN heißt,
-	 blitzt die blaue LED kurz, danach kann man mit der Kamera normal arbeiten...
-	 bei jedem Einschalten wir die AUTOEXEC.BIN geladen...
-	 Wenn man einfach einen SD-ROM-Dump draufkopiert und umbenennt, geht gar nix...
-	 
-	 */
-	
-	// DisableBootDisk()
-	/*  Funktion f = 0xFFD21260;
-	 f();
-	 *((volatile long*)LED_BLUE) = 0x46;*/
-	/* Damit kann man den Standard wiederherstellen, danach treten die obigen
-	 Probleme nicht mehr auf... */
-	
-}
-
-void CreateMyTask()
-{
-	CreateTask("MyTask2", 0x1A, 0x2000, MyTask2, 0);
-}
-
